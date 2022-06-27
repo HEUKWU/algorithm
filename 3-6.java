beebc122f8036b622bb78664184d30641e4476e9
Disaster <100930333+heuku@users.noreply.github.com>
2022-06-27 15:04:15 +0900

    Create 3-6.java

---
+++ 3-6.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 24;
+        System.out.println(num / 10 * 10 + 10 - num); // mine
+        System.out.println(10 - num % 10); // commentary 1
+        System.out.println((num / 10 + 1) * 10 - num); // commentary 2
+    }
+}

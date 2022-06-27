0ff9808d8eaebcc077264b5f1062aae5a9654a85
Disaster <100930333+heuku@users.noreply.github.com>
2022-06-27 15:04:34 +0900

    Update 3-5.java

--- 3-5.java
+++ 3-5.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int num = 333;
-        System.out.println(num / 10 + "1"); // 내 답
-        System.out.println(num / 10 * 10 + 1); // 교재 답
+        System.out.println(num / 10 + "1"); // mine
+        System.out.println(num / 10 * 10 + 1); // commentary
     }
 }

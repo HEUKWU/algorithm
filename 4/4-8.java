c9e2328dc16ab2cbbb40bbf1a18fe7396e7272d4
Disaster <100930333+heuku@users.noreply.github.com>
2022-07-02 20:08:18 +0900

    Create 4-8.java

---
+++ 4/4-8.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (2 * i + 4 * j == 10) {
+                    System.out.println("x = " + i + ", y = " + j);
+                }
+            }
+        }
+    }
+}

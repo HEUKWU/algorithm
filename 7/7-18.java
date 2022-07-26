a2909ab2913627f60d404309beff3c65c1a5cd60
Disaster <100930333+heuku@users.noreply.github.com>
2022-07-26 19:35:22 +0900

    Create 7-18.java

---
+++ 7/7-18.java
@@ -0,0 +1,45 @@
+public class Main {
+    static DanceRobot d;
+    static SingRobot s;
+    static DrawRobot dr;
+    public static void action(Robot robot) {
+        if (robot instanceof DanceRobot) {
+            d = (DanceRobot)robot;
+            d.dance();
+        } else if (robot instanceof SingRobot) {
+            s = (SingRobot)robot;
+            s.sing();
+        } else if (robot instanceof DrawRobot) {
+            dr = (DrawRobot)robot;
+            dr.draw();
+        }
+    }
+
+    public static void main(String[] args) {
+        Robot[] arr = {new DanceRobot(), new SingRobot(), new DrawRobot()};
+
+        for (int i = 0; i < arr.length; i++) {
+            action(arr[i]);
+        }
+    }
+}
+
+class Robot {}
+
+class DanceRobot extends Robot {
+    void dance() {
+        System.out.println("춤을 춥니다.");
+    }
+}
+
+class SingRobot extends Robot {
+    void sing() {
+        System.out.println("노래를 합니다.");
+    }
+}
+
+class DrawRobot extends Robot {
+    void draw() {
+        System.out.println("그림을 그립니다.");
+    }
+}

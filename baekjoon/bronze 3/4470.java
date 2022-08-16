a6aef0eac93e95009fb68cc64a5e0acaf5776fa5
Disaster <100930333+heuku@users.noreply.github.com>
2022-08-17 01:22:24 +0900

    Create 4470.java

---
+++ baekjoon/bronze 3/4470.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int index = i + 1;
+            System.out.println(index + ". " + arr[i]);
+        }
+    }
+}

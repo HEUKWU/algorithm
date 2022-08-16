25c9a2ec5cd1f11f81e7d727195b455455d63e71
Disaster <100930333+heuku@users.noreply.github.com>
2022-08-17 01:22:02 +0900

    Create 4458.java

---
+++ baekjoon/bronze 3/4458.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            String s1 = s.substring(0, 1).toUpperCase();
+            String s2 = s.substring(1);
+            System.out.println(s1 + s2);
+        }
+    }
+}
